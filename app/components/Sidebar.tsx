6cb113df946592872dad1032fd9b16cc38f0b030
Apmen03 <b.vardy03@gmail.com>
2025-02-06 20:26:48 +0400

    change fliend list style

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import styles from '../styles/sidebar.module.css';
 
-interface Friend {
-  id: string;
-  name: string;
-}
+const friends = [
+  { name: "Friend 1", photo: "/images/friend1.jpg", active: true },
+  { name: "Friend 2", photo: "/images/friend2.jpg", active: false },
+  { name: "Friend 3", photo: "/images/friend3.jpg", active: true },
+];
 
 const Sidebar: React.FC = () => {
-  const friends: Friend[] = [
-    { id: '1', name: 'Friend 1' },
-    { id: '2', name: 'Friend 2' },
-    { id: '3', name: 'Friend 3' },
-  ];
-
   return (
     <aside className={styles.sidebar}>
       <h2>Friends</h2>
-      <ul>
-        {friends.length > 0 ? (
-          friends.map((friend) => (
-            <li key={friend.id}>{friend.name}</li>
-          ))
-        ) : (
-          <li>No friends yet</li>
-        )}
+      <ul className={styles.friendList}>
+        {friends.map((friend, index) => (
+          <li key={index} className={styles.friendItem}>
+            <div className={styles.friendPhotoContainer}>
+              <img
+                src={friend.photo}
+                alt={friend.name}
+                className={styles.friendPhoto}
+              />
+              <div
+                className={`${styles.statusIndicator} ${friend.active ? styles.active : styles.inactive}`}
+              ></div>
+            </div>
+            <span className={styles.friendName}>{friend.name}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
